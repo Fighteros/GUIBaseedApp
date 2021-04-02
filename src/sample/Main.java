8de51b1f8e25d5779fe620098281fa7c674bca16
fighteros <am5419@fayoum.edu.eg>
2021-04-03 00:09:22 +0200

    HBOX and VBOX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,15 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -12,17 +17,38 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        BorderPane borderPane = new BorderPane();
-        borderPane.setTop(new CustomPane("Top"));
-        borderPane.setRight(new CustomPane("Right"));
-        borderPane.setBottom(new CustomPane("Bottom"));
-        borderPane.setLeft(new CustomPane("Left"));
-        borderPane.setCenter(new CustomPane("Center"));
+        BorderPane brdPane = new BorderPane();
+        brdPane.setTop(getHbox());
+        brdPane.setLeft(getVbox());
 
-        Scene scene = new Scene(borderPane, 200, 250);
-        primaryStage.setTitle("borderPane");
-        primaryStage.setScene(scene);
+        Scene scn = new Scene(brdPane);
+        primaryStage.setTitle("ShowHboxVbox");
+        primaryStage.setScene(scn);
         primaryStage.show();
+    }
+
+    private HBox getHbox(){
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(15, 15, 15, 15));
+        hBox.setStyle("-fx-background-color: gold");
+        hBox.getChildren().add(new Button("Computer Science"));
+        hBox.getChildren().add(new Button("Chemistry"));
+        ImageView imageView = new ImageView(new Image("sample/image/us.gif"));
+        hBox.getChildren().add(imageView);
+        return hBox;
+    }
+
+    private VBox getVbox() {
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(15, 5, 5, 5));
+        vBox.getChildren().add(new Label("Courses"));
+        Label[] courses = {new Label("CSCI 1301"), new Label("CSCI 1302"), new Label("CSCI 2410"), new Label("CSCI 3720")};
+        for (Label course : courses) {
+            vBox.setMargin(course, new Insets(0, 0, 0, 15));
+            vBox.getChildren().add(course);
+        }
+        return vBox;
+
     }
 
 
