de14b2b9426e1d00013b9e73e3f318fc65380458
fighteros <am5419@fayoum.edu.eg>
2021-04-03 00:56:03 +0200

    Text ShowText

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,54 +3,39 @@ package sample;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) {
-        BorderPane brdPane = new BorderPane();
-        brdPane.setTop(getHbox());
-        brdPane.setLeft(getVbox());
+        Pane pane = new Pane();
+        pane.setPadding(new Insets(5, 5, 5, 5));
+        Text txt = new Text(20, 20, "programming is fun!");
+        txt.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.ITALIC, 15));
+        pane.getChildren().add(txt);
 
-        Scene scn = new Scene(brdPane);
-        primaryStage.setTitle("ShowHboxVbox");
-        primaryStage.setScene(scn);
+        Text txt0 = new Text(60, 60, "Programming is Fun\n DisplayTexy");
+        pane.getChildren().add(txt0);
+        Text txt1 = new Text(10, 100, "Programming is Fun\n DisplayTexy");
+        txt1.setFill(Color.RED);
+        txt1.setUnderline(true);
+        txt1.setStrikethrough(true);
+        pane.getChildren().add(txt1);
+
+        primaryStage.setTitle("ShowText");
+        primaryStage.setScene(new Scene(pane));
         primaryStage.show();
-    }
 
-    private HBox getHbox(){
-        HBox hBox = new HBox();
-        hBox.setPadding(new Insets(15, 15, 15, 15));
-        hBox.setStyle("-fx-background-color: gold");
-        hBox.getChildren().add(new Button("Computer Science"));
-        hBox.getChildren().add(new Button("Chemistry"));
-        ImageView imageView = new ImageView(new Image("sample/image/us.gif"));
-        hBox.getChildren().add(imageView);
-        return hBox;
-    }
 
-    private VBox getVbox() {
-        VBox vBox = new VBox();
-        vBox.setPadding(new Insets(15, 5, 5, 5));
-        vBox.getChildren().add(new Label("Courses"));
-        Label[] courses = {new Label("CSCI 1301"), new Label("CSCI 1302"), new Label("CSCI 2410"), new Label("CSCI 3720")};
-        for (Label course : courses) {
-            vBox.setMargin(course, new Insets(0, 0, 0, 15));
-            vBox.getChildren().add(course);
-        }
-        return vBox;
 
     }
-
 
     public static void main(String[] args) {
         launch(args);
