2ad41355df0e7bea326a1caf954de8727b8db6ff
fighteros <am5419@fayoum.edu.eg>
2021-04-02 20:25:42 +0200

    finished CustomPane

--- src/sample/CustomPane.java
+++ src/sample/CustomPane.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.StackPane;
 public class CustomPane extends StackPane {
     public CustomPane(String s) {
         getChildren().add(new Label(s));
-        setStyle("-fx-border-color: red");
+        setStyle("-fx-border-color: #6d2323");
         setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
     }
 }
